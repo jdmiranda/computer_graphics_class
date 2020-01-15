9daa95300664f17b59df69c662321cda76f86785
michaeljayl <mjl@nova.edu>
2020-01-14 19:48:15 -0500

    Add files via upload

--- graphics/js/earthSunA.js
+++ graphics/js/earthSunA.js
@@ -32,7 +32,7 @@ function createScene() {
 
 
 # HAD BEEN '../assets/earth_4k.jpg'
-function createEarth(radius=2, rps=0.1, earthTexture='assets/earth_4k.jpg') {
+function createEarth(radius=2, rps=0.1, earthTexture='graphics/assets/earth_4k.jpg') {
     let geom = new THREE.SphereGeometry(radius, 48, 48);
     let texture = getTexture(earthTexture);
     let matArgs = {map: texture,  specular: 0xFF9999, shininess: 10};
