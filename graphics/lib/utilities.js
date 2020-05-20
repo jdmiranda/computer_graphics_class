2b1dcf332b4c74c41b853c5bf0752b79834aec4d
michaeljayl <mjl@nova.edu>
2020-05-19 20:32:08 -0400

    Add files via upload

--- graphics/lib/utilities.js
+++ graphics/lib/utilities.js
@@ -38,6 +38,15 @@ function getRandomColor(minSaturation, minLightness, maxLightness) {
   return new THREE.Color().setHSL(hue, sat, lit);
 }
 
+let colorVals = [0xffffff, 0xffff66, 0xff66ff, 0x66ffff, 0xff66666, 0x66ff66, 0x6666ff];
+
+function getRandomBrightColor() {
+  let color = new THREE.Color();
+  let i = getRandomInt(0, 6);
+  color.setHex((Math.random()/2 + 0.5) * colorVals[i]);
+  return color;
+}
+
 
 function rpsToRadians(rps, t) {
   return 2.0 * Math.PI * rps * t;
@@ -170,5 +179,55 @@ function makeAxes(params) {
   theAxes.add(arrowY);
   theAxes.add(arrowZ);
   return theAxes;
+}
+
+
+function makeAxis(params) {
+  var theAxis = new THREE.Object3D();
+
+  params = params || {};
+  // xyz: 0 (X); 1 (Y); 2 (Z)
+  var xyz = params.xyz !== undefined ? params.xyz : 1;
+  var axisRadius = params.axisRadius !== undefined ? params.axisRadius:0.04;
+  var axisLength = params.axisLength !== undefined ? params.axisLength:11;
+  var axisTess = params.axisTess !== undefined ? params.axisTess:48;
+  var color = params.color !== undefined ? params.color : 0xff0000;
+  var material = params.material !== undefined ? params.material : new THREE.MeshLambertMaterial({color:color});
+  material.side = THREE.DoubleSide;
+ 
+  var axis = new THREE.Mesh(
+    new THREE.CylinderGeometry(axisRadius, axisRadius, axisLength, axisTess, 1, true), 
+    material
+    );
+  if (xyz == 0) { // X
+    axis.rotation.z = - Math.PI / 2;
+    axis.position.x = axisLength / 2;
+  } else if (xyz == 1) { // Y
+    axis.position.y = axisLength / 2;
+  }
+  else { // Z
+    axis.rotation.y = - Math.PI / 2;
+    axis.rotation.z = - Math.PI / 2;
+    axis.position.z = axisLength / 2-0; 
+  }
+  theAxis.add(axis);
+
+  var arrow = new THREE.Mesh(
+    new THREE.CylinderGeometry(0, 4*axisRadius, 4*axisRadius, axisTess, 1, true), 
+    material
+    );
+  if (xyz == 0) {
+    arrow.rotation.z = - Math.PI / 2;
+    arrow.position.x = axisLength - 0 + axisRadius*4/2;
+  } else if (xyz == 1) {
+    arrow.position.y = axisLength + axisRadius*4/2; 
+  } else {
+    arrow.rotation.z = - Math.PI / 2;
+    arrow.rotation.y = - Math.PI / 2;
+    arrow.position.z = axisLength + axisRadius*4/2; 
+  }
+  theAxis.add(arrow
+    );
+  return theAxis;
 }
 
