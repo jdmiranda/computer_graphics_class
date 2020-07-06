f1a950b9024add9e3e07fd4b5a66cf54fbb7fc97
michaeljayl <mjl@nova.edu>
2020-07-06 13:08:22 -0400

    Add files via upload

--- graphicsN/js/threeTorusReplFN.js
+++ graphicsN/js/threeTorusReplFN.js
@@ -43,7 +43,7 @@ function createScene() {
     let light = new THREE.PointLight(0xFFFFFF, 1.0, 10000);
     light.position.set(100, 200, 200);
     let light2 = new THREE.PointLight(0xFFFFFF, 1.0, 1000 );
-    light2.position.set(-100, 200, -200);
+    light2.position.set(-100, -200, -200);
     let ambientLight = new THREE.AmbientLight(0x111111);
     scene.add(light);
     scene.add(light2);
